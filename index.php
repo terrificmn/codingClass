955426c49d049b15afabd7f5aa88730506186a05
root <root@localhost.localdomain>
2021-01-12 18:32:35 -0800

    first php commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="UTF-8" />
+	<title>indexphp</title>
+	</head>
+<body>
+
+<?php
+	echo ('Hello World');
+?>
+	<br>
+	<a href="projects/2010/index.php">OLD 2010 portfolio in Vancouver</a><br>
+	<a href="projects/kall/index.php">KA SOLUTION</a><br>
+	<a href="projects/restart/index.php">I quited the job cause COVID19 and started to create my blog again.</a><br>
+	<a href="phpLesson/">php tutorials: 주소는 직접 입력하시오</a><br>
+
+<?php
+phpinfo();
+?>
+
+</body>
+</html>
